2806c7bab0995a633c1b45857545f06ae9b06650
George Karagkiaouris <karaggeorge0@gmail.com>
2019-07-17 17:32:09 -0400

    Fix style flashing

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,15 @@
-// _document is only rendered on the server side and not on the client side
-// Event handlers like onClick can't be added to this file
+import Document, { Head, Main, NextScript } from 'next/document'
 
-// ./pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </html>
     )
   }
 }
-
-export default MyDocument
